0c59aaa01047146bab67237346f88aba4c98eb0c
zeib01 <ibragimovazeinep@gmail.com>
2024-03-27 19:01:16 +0100

    all tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,48 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+const getUserNames = users => {
+    return users.map(user => user.name);
 }
-let result = makeTransaction(5, 3000);
-console.log("🚀 ~ result:", result);
 
-result = makeTransaction(3, 1000);
-console.log("🚀 ~ result:", result);
 
-result = makeTransaction(10, 500);
-console.log("🚀 ~ result:", result);
+console.log(
+    getUserNames([
+        {
+            name: "Moore Hensley",
+            email: "moorehensley@indexia.com",
+            balance: 2811
+        },
+        {
+            name: "Sharlene Bush",
+            email: "sharlenebush@tubesys.com",
+            balance: 3821
+        },
+        {
+            name: "Ross Vazquez",
+            email: "rossvazquez@xinware.com",
+            balance: 3793
+        },
+        {
+            name: "Elma Head",
+            email: "elmahead@omatom.com",
+            balance: 2278
+        },
+        {
+            name: "Carey Barr",
+            email: "careybarr@nurali.com",
+            balance: 3951
+        },
+        {
+            name: "Blackburn Dotson",
+            email: "blackburndotson@furnigeer.com",
+            balance: 1498
+        },
+        {
+            name: "Sheree Anthony",
+            email: "shereeanthony@kog.com",
+            balance: 2764
+        },
+    ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
 
 
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,79 @@
-function getElementWidth(content, padding, border) {
-
-    return Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2;
+const sortByDescendingFriendCount = users => {
+    return users.toSorted((a, b) => b.friends.length - a.friends.length);
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(
+    sortByDescendingFriendCount([
+        {
+            name: "Moore Hensley",
+            friends: ["Sharron Pace"],
+            gender: "male"
+        },
+        {
+            name: "Sharlene Bush",
+            friends: ["Briana Decker", "Sharron Pace"],
+            gender: "female"
+        },
+        {
+            name: "Ross Vazquez",
+            friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+            gender: "male"
+        },
+        {
+            name: "Elma Head",
+            friends: ["Goldie Gentry", "Aisha Tran"],
+            gender: "female"
+        },
+        {
+            name: "Carey Barr",
+            friends: ["Jordan Sampson", "Eddie Strong"],
+            gender: "male"
+        },
+        {
+            name: "Blackburn Dotson",
+            friends: ["Jacklyn Lucas", "Linda Chapman"],
+            gender: "male"
+        },
+        {
+            name: "Sheree Anthony",
+            friends: ["Goldie Gentry", "Briana Decker"],
+            gender: "female"
+        }
+    ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
